3e346e5a38755995704dc0034b348db1a9b0e4af
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2023-12-26 07:27:59 -0500

    hands on samples

--- rxjs-handson/src/index.ts
+++ rxjs-handson/src/index.ts
@@ -2,8 +2,10 @@ import { loadFiles$ } from 'observable-dir';
 
 const files$ = loadFiles$('\\data');
 
-files$.subscribe({
+const subscriber = files$.subscribe({
   next: (files) => console.log(files),
   error: (err) => console.error(err),
   complete: () => console.log('done'),
 });
+
+subscriber.unsubscribe();
